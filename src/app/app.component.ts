40fc1aa4c70eaa3ac64bf0270d71cda50dd769f8
MatthewBald <matt.e.bald@gmail.com>
2017-08-02 12:41:16 -0600

    Using a minimal project instead. Added routing module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>
+      Welcome to {{title}}!!
+    </h1>
+    <router-outlet></router-outlet>
+  `,
+  styles: []
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app';
 }
